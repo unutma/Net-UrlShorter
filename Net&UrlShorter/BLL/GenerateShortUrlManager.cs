9a2e02a20f6ff36b36af45bfdb1428ae245ddc7a
Muharrem YILDIZ <33318914+unutma@users.noreply.github.com>
2023-04-10 16:47:23 +0300

    send
    send

---
+++ Net&UrlShorter/BLL/GenerateShortUrlManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Net_UrlShorter.BLL
+{
+	public class GenerateShortUrlManager
+	{
+        public string GenerateShortUrl()
+        {
+            string characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            Random random = new Random();
+            char[] shortUrl = new char[6];
+
+            for (int i = 0; i < 6; i++)
+            {
+                shortUrl[i] = characters[random.Next(characters.Length)];
+            }
+            return new string(shortUrl);
+        }
+    }
+}
+
---
+++ Net&UrlShorter/Controllers/ShortenController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Net_UrlShorter.BLL;
+
+namespace Net_UrlShorter.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ShortenController : ControllerBase
+    {
+        private static Dictionary<string, string> _urlMapping = new Dictionary<string, string>();
+
+        [HttpPost]
+        public IActionResult Shorten([FromBody] string url)
+        {
+            if (string.IsNullOrEmpty(url))
+            {
+                return BadRequest("Input URL cannot be empty.");
+            }
+            Uri uriResult;
+            bool isValidUrl = Uri.TryCreate(url, UriKind.Absolute, out uriResult)
+                && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps);
+            if (!isValidUrl)
+            {
+                return BadRequest("Invalid URL format.");
+            }
+            GenerateShortUrlManager generateShortUrlMAnager = new();
+            string shortUrl = generateShortUrlMAnager.GenerateShortUrl();
+            _urlMapping.Add(shortUrl, url);
+
+            return Ok($"Shortened URL: https://short.com/{shortUrl}");
+        }
+    }
+}
